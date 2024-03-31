65e2c4a3130a034a488b3e8f297dd02217cb7fa9
GPark <qkrwur724@naver.com>
2024-03-31 13:46:09 +0900

    1

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+	const elements = document.getElementsByClassName('lead mb-5');
+	Array.from(elements).forEach(element => {
+		new TypeIt(element, {
+			speed: 60,
+			waitUntilVisible: true,
+			afterComplete: function (instance) {
+				instance.destroy();
+			}
+		}).go();
+	});
+
+
+});
+
